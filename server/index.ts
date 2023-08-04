e0aaf6656842dd2da19a5eecaf7020e57e6e7cde
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-08-04 22:46:27 +0200

    basic trpc setup

--- server/index.ts
+++ server/index.ts
@@ -15,6 +15,107 @@ import { deriveProjectFrom } from './path'
 
 const isProduction = process.env['NODE_ENV'] === 'production'
 
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { inferAsyncReturnType, initTRPC, TRPCError } from '@trpc/server'
+import { EventEmitter } from 'events'
+import { z } from 'zod'
+
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => {
+  const getUser = () => {
+    if (req.headers.authorization !== 'secret') {
+      return null
+    }
+    return {
+      name: 'alex',
+    }
+  }
+
+  return {
+    req,
+    res,
+    user: getUser(),
+  }
+}
+
+type Context = inferAsyncReturnType<typeof createContext>
+
+const t = initTRPC.context<Context>().create()
+const router = t.router
+const publicProcedure = t.procedure
+
+let id = 0
+
+const ee = new EventEmitter()
+const db = {
+  posts: [
+    {
+      id: ++id,
+      title: 'hello',
+    },
+  ],
+  messages: [createMessage('initial message')],
+}
+
+function createMessage(text: string) {
+  const msg = {
+    id: ++id,
+    text,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  }
+  ee.emit('newMessage', msg)
+  return msg
+}
+
+const postRouter = router({
+  createPost: t.procedure.input(z.object({ title: z.string() })).mutation(({ input }) => {
+    const post = {
+      id: ++id,
+      ...input,
+    }
+    db.posts.push(post)
+    return post
+  }),
+  listPosts: publicProcedure.query(() => db.posts),
+})
+
+const messageRouter = router({
+  addMessage: publicProcedure.input(z.string()).mutation(({ input }) => {
+    const msg = createMessage(input)
+    db.messages.push(msg)
+
+    return msg
+  }),
+  listMessages: publicProcedure.query(() => db.messages),
+})
+
+// root router to call
+const appRouter = router({
+  // merge predefined routers
+  post: postRouter,
+  message: messageRouter,
+  // or individual procedures
+  hello: publicProcedure.input(z.string().nullish()).query(({ input, ctx }) => {
+    return `👋hello ${input ?? ctx.user?.name ?? 'world'}`
+  }),
+  // or inline a router
+  admin: router({
+    secret: publicProcedure.query(({ ctx }) => {
+      if (!ctx.user) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
+      }
+      if (ctx.user?.name !== 'alex') {
+        throw new TRPCError({ code: 'FORBIDDEN' })
+      }
+      return {
+        secret: 'sauce',
+      }
+    }),
+  }),
+})
+
+export type AppRouter = typeof appRouter
+
 const configuration = loadConfigurationFrom(process.env)
 if (configuration === 'failed') {
   console.error('missing configuration')
@@ -35,6 +136,14 @@ async function startServer() {
   const app = express()
 
   app.use(compression())
+
+  app.use(
+    '/api/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    }),
+  )
 
   // Vite integration
   if (isProduction) {
--- src/pages/about/+Page.tsx
+++ src/pages/about/+Page.tsx
@@ -1,7 +1,23 @@
-import '../../code.css'
+import { useEffect, useState } from 'react'
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import { Counter } from './Counter'
+import type { AppRouter } from '../../../server'
+import '../../code.css'
+
+const client = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/api/trpc',
+    }),
+  ],
+})
 
 function Page() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    client.hello.query('Berry').then((data) => setData(data))
+  }, [])
   return (
     <>
       <h1>About</h1>
@@ -13,6 +29,10 @@ function Page() {
         <li>
           Interactive. <Counter />
         </li>
+      </ul>
+      <ul>
+        <li>trpc</li>
+        <li>{data}</li>
       </ul>
     </>
   )
