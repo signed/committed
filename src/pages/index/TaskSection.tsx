231f64b34f48f77f3fb88ff8203482a00dd2adaa
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-11-05 21:20:48 +0100

    render top level tasks

--- src/pages/index/TaskSection.tsx
+++ src/pages/index/TaskSection.tsx
@@ -1,8 +1,24 @@
-import { Task } from '../../core/ReleaseTasks'
+import { genericTaskSummary, Task, testTaskSummary } from '../../core/ReleaseTasks'
 
 export type TaskSectionProperties = {
   tasks: Task[]
 }
+
+function summaryFor(task: Task): string {
+  switch (task.type) {
+    case 'test':
+      return testTaskSummary(task)
+    case 'generic':
+      return genericTaskSummary(task)
+  }
+}
+
 export const TaskSection = (_props: TaskSectionProperties) => {
-  return <div>Implement them tasks</div>
+  return (
+    <ol>
+      {_props.tasks.map((task) => (
+        <li key={'k'}>{summaryFor(task)}</li>
+      ))}
+    </ol>
+  )
 }
