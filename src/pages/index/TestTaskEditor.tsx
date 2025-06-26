5cd0a0e4c71d74bee8adad816389a4194a9c0c24
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-06-26 23:20:44 +0200

    local variable

--- src/pages/index/TestTaskEditor.tsx
+++ src/pages/index/TestTaskEditor.tsx
@@ -15,38 +15,41 @@ export function TestTaskEditor(props: TestTaskEditorProperties) {
     <>
       {testTaskSummary(task)}
       <ul>
-        {task.ticketTests.map((ticketTest) => (
-          <li key={ticketTest.ticket.identifier}>
-            <StatusToggle
-              status={ticketTest.status}
-              onChange={async (status) => {
-                const identifier = ticketTest.ticket.identifier
-                await client.ticketTest.setStatus
-                  .mutate({ identifier, status })
-                  .then(() => {
-                    window.location.reload()
-                  })
-                  .catch((e) => console.log(e))
-              }}
-            />
-            {' ' + ticketToShortString(ticketTest.ticket)}
-            {ticketTest.ticket.kind === 'ticket' && <ExternalLink destination={ticketTest.ticket.url} />}
-            {' ' + testersToString(ticketTest.testers)}
-            <TestersSelect
-              availableTesters={props.testers}
-              assignedTesters={ticketTest.testers}
-              onChange={async (testers) => {
-                const identifier = ticketTest.ticket.identifier
-                await client.ticketTest.setTesters
-                  .mutate({ identifier, testers })
-                  .then(() => {
-                    window.location.reload()
-                  })
-                  .catch((e) => console.log(e))
-              }}
-            />
-          </li>
-        ))}
+        {task.ticketTests.map((ticketTest) => {
+          const ticket = ticketTest.ticket
+          return (
+            <li key={ticket.identifier}>
+              <StatusToggle
+                status={ticketTest.status}
+                onChange={async (status) => {
+                  const identifier = ticket.identifier
+                  await client.ticketTest.setStatus
+                    .mutate({ identifier, status })
+                    .then(() => {
+                      window.location.reload()
+                    })
+                    .catch((e) => console.log(e))
+                }}
+              />
+              {' ' + ticketToShortString(ticket)}
+              {ticket.kind === 'ticket' && <ExternalLink destination={ticket.url} />}
+              {' ' + testersToString(ticketTest.testers)}
+              <TestersSelect
+                availableTesters={props.testers}
+                assignedTesters={ticketTest.testers}
+                onChange={async (testers) => {
+                  const identifier = ticket.identifier
+                  await client.ticketTest.setTesters
+                    .mutate({ identifier, testers })
+                    .then(() => {
+                      window.location.reload()
+                    })
+                    .catch((e) => console.log(e))
+                }}
+              />
+            </li>
+          )
+        })}
       </ul>
     </>
   )
